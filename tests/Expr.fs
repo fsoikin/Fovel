574704db683ce44ace7df3f24cbe79bf0f07d1e0
Fyodor Soikin <name.fa@gmail.com>
2016-01-29 20:24:11 -0500

    Add tests for mapIntrinsic.

--- tests/Expr.fs
+++ tests/Expr.fs
@@ -10,20 +10,26 @@ module Common =
   let mapIsDual mapTo mapFrom input = 
     (input |> mapTo string |> mapFrom int) = input
 
-module Expr =
-  open Expr
-  let [<Fact>] ``mapType should be its own dual`` () = Check.Quick (mapIsDual mapType mapType)
-  let [<Fact>] ``mapSymbol should be its own dual`` () = Check.Quick (mapIsDual mapSymbol mapSymbol)
-
   let additiveProp getAll exprs = 
     let a = exprs |> List.collect getAll |> List.distinct
     let b = E.NewTuple (0, exprs) |> getAll |> List.distinct
     a = b
 
+module Expr =
+  open Expr
+  let [<Fact>] ``mapType should be its own dual`` () = Check.Quick (mapIsDual mapType mapType)
+  let [<Fact>] ``mapSymbol should be its own dual`` () = Check.Quick (mapIsDual mapSymbol mapSymbol)
+  let [<Fact>] ``mapIntrinsic should be its own dual`` () = Check.Quick (mapIsDual mapIntrinsic mapIntrinsic)
+
   let [<Fact>] ``allTypes should be additive`` () = Check.Quick (additiveProp allTypes)
   let [<Fact>] ``allSymbols should be additive`` () = Check.Quick (additiveProp allSymbols)
+
 
 module Binding =
   open Binding
   let [<Fact>] ``mapType should be its own dual`` () = Check.Quick (mapIsDual mapType mapType)
   let [<Fact>] ``mapSymbol should be its own dual`` () = Check.Quick (mapIsDual mapSymbol mapSymbol)
+  let [<Fact>] ``mapIntrinsic should be its own dual`` () = Check.Quick (mapIsDual mapIntrinsic mapIntrinsic)
+//
+//  let [<Fact>] ``allTypes should be additive`` () = Check.Quick (additiveProp allTypes)
+//  let [<Fact>] ``allSymbols should be additive`` () = Check.Quick (additiveProp allSymbols)
